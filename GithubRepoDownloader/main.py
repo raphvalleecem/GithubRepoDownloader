c412856076e00f4dab3bc9d09b5579f390ea1067
Raphael Vallée <2263456@cegepmontpetit.ca>
2023-11-17 14:37:00 -0500

    WIP - Add Private Repo support

--- GithubRepoDownloader/main.py
+++ GithubRepoDownloader/main.py
@@ -5,37 +5,50 @@ from os.path import expanduser
 import requests
 
 
-def download_repo_zip(user, repo_name, downloadpath):
+def download_repo_zip(user, repo_name, download_path, token):
     zip_url = f"https://github.com/{user}/{repo_name}/archive/master.zip"
-    response = requests.get(zip_url, stream=True)
-    zip_filename = f"{downloadpath}/{repo_name}.zip"
+    headers = {'Authorization': f'token {token}'}
+    response = requests.get(zip_url, headers=headers, stream=True)
 
-    with open(zip_filename, 'wb') as zip_file:
-        shutil.copyfileobj(response.raw, zip_file)
+    if response.status_code == 200:
+        zip_filename = f"{download_path}/{repo_name}.zip"
+        with open(zip_filename, 'wb') as zip_file:
+            shutil.copyfileobj(response.raw, zip_file)
+        return zip_filename
+    else:
+        print(f"Failed to download {repo_name}. Status code: {response.status_code}")
+        return None
 
-    return zip_filename
 
-
-def download_all_repos(username, download_path):
+def download_all_repos(username, download_path, github_token):
     # Create the download directory if it doesn't exist
     os.makedirs(download_path, exist_ok=True)
 
     # Get the list of repositories for the given user
-    api_url = f"https://api.github.com/users/{username}/repos"
-    response = requests.get(api_url)
-    repos = response.json()
+    api_url = f"https://api.github.com/user/repos"
+    headers = {'Authorization': f'token {github_token}'}
+    response = requests.get(api_url, headers=headers)
 
-    # Download each repository as a zip file
-    for repo in repos:
-        repo_name = repo['name']
-        print(f"Downloading {repo_name}...")
-        zip_filename = download_repo_zip(username, repo_name, download_path)
-        print(f"{repo_name} downloaded to {zip_filename}")
+    if response.status_code == 200:
+        repos = response.json()
+
+        # Download each repository as a zip file
+        for repo in repos:
+            repo_name = repo['name']
+            print(f"Downloading {repo_name}...")
+            zip_filename = download_repo_zip(username, repo_name, download_path, github_token)
+            if zip_filename:
+                print(f"{repo_name} downloaded to {zip_filename}")
+            else:
+                print(f"Failed to download {repo_name}.")
+    else:
+        print(f"Failed to retrieve repositories. Status code: {response.status_code}")
 
 
 if __name__ == "__main__":
     username = "Tranzitron"
     home = expanduser("~")
     download_path = fr"{home}\Desktop\{username}_repos"
+    github_token = "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"
 
-    download_all_repos(username, download_path)
+    download_all_repos(username, download_path, github_token)
